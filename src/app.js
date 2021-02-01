2a32ee04366464a3f99bf6e1a9247d5f08820586
Evraa <evramyousef@gmail.com>
2021-02-01 18:08:00 +0200

    addedL views with ejs

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ const path = require('path');
 //express app
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs');
+//where to look for it?
+app.set('views', '../views'); //default is views
+
+
 //listen for requests
 const server = app.listen(3000);
 
@@ -13,14 +19,26 @@ app.get('/',(req, res)=>{
     //which comes in handy with express
     //it inferes the type of conten we send back (json/html)
     //manually sets the status code for us
-    res.sendFile(path.resolve('../views/index.html'));
+    // res.sendFile(path.resolve('../views/index.html'));
+    
+    const blogs =[
+        {title: 'blog 1', snippet:'snippet 1'},
+        {title: 'blog 2', snippet:'snippet 2'},
+        {title: 'blog 3', snippet:'snippet 3'}
+    ];
+    //when using views, we use render method
+    res.render('index', {page_title:'home',blogs}); 
 });
 
 app.get('/about',(req, res)=>{
     // res.sendFile('../views/about.html', {root: __dirname});
-    res.sendFile(path.resolve('../views/about.html'));
+    // res.sendFile(path.resolve('../views/about.html'));
+    res.render('about',{page_title:'About'});
 });
 
+app.get('/blogs/create', (req, res)=>{
+    res.render('create',{page_title:'Create'});
+});
 
 //REDIRECT
 app.get('/about-us',(req, res)=>{
@@ -33,6 +51,7 @@ app.get('/about-us',(req, res)=>{
 
 app.use((req, res)=>{
     //you need to manully state its an error status code.
-    res.status(404).sendFile(path.resolve('../views/404.html'));
+    // res.status(404).sendFile(path.resolve('../views/404.html'));
+    res.status(404).render('404',{page_title:'Error'});
 });
 
--- src/readme.md
+++ src/readme.md
@@ -1,10 +1,14 @@
 ## What to learn?
     + express with view engines (EJS)
-
+    + pass data from server
+    + using view engine
+    + grouping common code
+    + adding css
 
 ## Keywords
     + views (EJS)
-
+    + render
+    + dynamic values
 
 ## Requirement (npm install)
 `sudo npm install -g nodemon`
